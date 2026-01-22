859f35eb7563bff13d99d5d5b28f2107413f8ee1
guih0113 <gui.henriquess13@gmail.com>
2026-01-22 18:49:25 -0300

    fix: import error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './app.tsx'
+import App from './App'
 
 const queryClient = new QueryClient()
 
